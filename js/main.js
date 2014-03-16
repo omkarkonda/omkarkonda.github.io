14aa0b35303d4aca5e8bd81e60699ff4ce7e625e
omkarkonda <omkar.konda@gmail.com>
2014-03-17 01:17:48 +0530

    portfolio images added

--- js/main.js
+++ js/main.js
@@ -45,5 +45,45 @@ $(
         })
 
     })
-)
+);
+
+$(document).ready(function() {
+    $('.gallery').magnificPopup({
+          delegate: 'a', // child items selector, by clicking on it popup will open
+          type: 'image'
+      // other options
+    });  
+    
+    var $container 	= $('#am-container'),
+					$imgs		= $container.find('img').hide(),
+					totalImgs	= $imgs.length,
+					cnt			= 0;
+				
+				$imgs.each(function(i) {
+					var $img	= $(this);
+					$('<img/>').load(function() {
+						++cnt;
+						if( cnt === totalImgs ) {
+							$imgs.show();
+							$container.montage({
+								fillLastRow				: true,
+								alternateHeight			: false,
+								alternateHeightRange	: {
+									min	: 100,
+									max	: 150
+								},
+                                margin : 5
+							});
+							
+							/* 
+							 * just for this demo:
+							 */
+							//$('#overlay').fadeIn(500);
+						}
+					}).attr('src',$img.attr('src'));
+				});	
+
+    
+    
+});
 
