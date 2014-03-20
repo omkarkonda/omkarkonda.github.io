94798442804713cb053e5882ab12ad8815c01bce
omkarkonda <omkar.konda@gmail.com>
2014-03-21 03:00:01 +0530

    packages add

--- js/main.js
+++ js/main.js
@@ -20,30 +20,8 @@ $(
 
         /* Every time the window is scrolled ... */
     $(window).scroll( function(){
-        console.log(scrollY);
         
-        if(scrollY>40){
-            $('.aboutme').addClass('sticky');
-        }else{
-            $('.aboutme').removeClass('sticky');
-        }
-        
-        
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-
-            }
-
-        })
-
+       
     })
 );
 
@@ -83,6 +61,16 @@ $(document).ready(function() {
 					}).attr('src',$img.attr('src'));
 				});	
 
+    $('.hero nav.wide').waypoint('sticky');
+    
+    
+    
+    $('nav.mobile .icon').on('click', function(ev){        
+              
+        $('nav.mobile').toggleClass('bounceInDown animated');        
+        $('nav.mobile ul').toggleClass('show');
+        
+    });
     
     
 });
